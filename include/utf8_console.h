7e65f2cd45750e362f7291f399fb8cb39f1f9221
Charlie Jiang <w@chariri.moe>
2022-02-24 11:24:53 +0800

    Expose utf8_cswidth

--- include/utf8_console.h
+++ include/utf8_console.h
@@ -12,6 +12,13 @@
 void initialize_utf8_console();
 
 /// <summary>
+/// Get display width of the specified [str] on console.
+/// i.e. UTF-8 version of the POSIX wcswidth function.
+/// Using width of a half-width latin letter as the unit.
+/// </summary>
+size_t utf8_cswidth(std::string_view str);
+
+/// <summary>
 /// setw I/O stream manipulator replacement for UTF-8 string,
 /// will handle character width properly (hopefully).
 /// </summary>
